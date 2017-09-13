997ee941c1f37c3ea7216cc41d56dbe717d7d26d
joyc <joyc@live.jp>
2017-09-13 23:49:51 +0900

    update template

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -147,8 +147,8 @@ if os.getcwd() == '/app':
 
     # 静态配置
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+    # STATIC_URL = '/static/'
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )
