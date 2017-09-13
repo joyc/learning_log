755a57472cc05b054067a00c0767413517e5cb65
joyc <joyc@live.jp>
2017-09-13 23:57:14 +0900

    update template

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -146,9 +146,14 @@ if os.getcwd() == '/app':
     DEBUG = False
 
     # 静态配置
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
-    # STATIC_URL = '/static/'
+    # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/1.9/howto/static-files/
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+    STATIC_URL = '/static/'
+    # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
+        os.path.join(PROJECT_ROOT, 'static'),
     )
